108f86522a09f57e98ba410eb5897285eb72a706
JL <julien.leuenberger@outlook.com>
2024-12-17 14:38:59 +0100

    Renamed `example.py` to `example_1.py`

---
+++ example_1.py
@@ -0,0 +1,35 @@
+from interface import Agent, main
+
+
+if __name__ == '__main__':
+    
+    agents = [
+        Agent("Alpha", "random"),
+        Agent("Bravo", "random"),
+        Agent("Charlie", "random"),
+        Agent("Delta", "random"),
+        Agent("Echo", "random"),
+        Agent("Foxtrot", "random"),
+        Agent("Golf", "random"),
+        Agent("Hotel", "random"),
+        Agent("India", "random"),
+        Agent("Juliet", "random"),
+        Agent("Kilo", "random"),
+        Agent("Lima", "random"),
+        Agent("Mike", "random"),
+        Agent("November", "random"),
+        Agent("Oscar", "random"),
+        Agent("Papa", "random"),
+        Agent("Quebec", "random"),
+        Agent("Romeo", "random"),
+        Agent("Sierra", "random"),
+        Agent("Tango", "random"),
+        Agent("Uniform", "random"),
+        Agent("Victor", "random"),
+        Agent("Whiskey", "random"),
+        Agent("Xray", "random"),
+        Agent("Yankee", "random"),
+        Agent("Zulu", "random"),
+    ]
+    
+    main(agents)
