3318b21e8cc9bc20884cd67491a8c4195adcb0db
JL <julien.leuenberger@outlook.com>
2024-12-22 17:15:50 +0100

    Removed deprecated `dna` variable

--- game/interface.py
+++ game/interface.py
@@ -64,7 +64,6 @@ class Agent:
     def __init__(self, name: str, model: Literal["random", "ChatGPT", "cmd"] | Dict[str, float]):
         self.model = model
         self.name = name
-        self.dna = [random.random() for _ in range(100)]
         
         if model == "ChatGPT":
             self.client = OpenAI()
