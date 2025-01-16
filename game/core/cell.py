bfddfaa9b392590f8726e0c26f574c628b1b1ea7
soleilvermeil <julien.leuenberger@outlook.com>
2025-01-16 15:45:36 +0100

    Added `visibility_proba` parameter

--- game/core/cell.py
+++ game/core/cell.py
@@ -5,6 +5,7 @@ class Cell:
         icon: str,
         food_multiplier: int = 1,
         water_multiplier: int = 1,
+        visibility_proba: int = 0.5,
         weapon_proba_multiplier: int = 1,
         dangerous_weapon_proba: int = 0,
     ):
@@ -12,5 +13,6 @@ class Cell:
         self.icon = icon
         self.food_multiplier = food_multiplier
         self.water_multiplier = water_multiplier
+        self.visibility_proba = visibility_proba
         self.weapon_proba_multiplier = weapon_proba_multiplier
         self.dangerous_weapon_proba = dangerous_weapon_proba
