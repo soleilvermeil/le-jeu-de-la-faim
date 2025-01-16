1a0057e8fdc3bef7423ced41185867c86976adce
JL <julien.leuenberger@outlook.com>
2025-01-16 16:05:08 +0100

    Set up GitHub Actions for testing

---
+++ tests/test.py
@@ -0,0 +1,39 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from game.agents.random_agent import RandomAgent
+from game.interface import main
+
+
+if __name__ == '__main__':
+    
+    agents = [
+        RandomAgent("Alpha"),
+        RandomAgent("Bravo"),
+        RandomAgent("Charlie"),
+        RandomAgent("Delta"),
+        RandomAgent("Echo"),
+        RandomAgent("Foxtrot"),
+        RandomAgent("Golf"),
+        RandomAgent("Hotel"),
+        RandomAgent("India"),
+        RandomAgent("Juliet"),
+        RandomAgent("Kilo"),
+        RandomAgent("Lima"),
+        RandomAgent("Mike"),
+        RandomAgent("November"),
+        RandomAgent("Oscar"),
+        RandomAgent("Papa"),
+        RandomAgent("Quebec"),
+        RandomAgent("Romeo"),
+        RandomAgent("Sierra"),
+        RandomAgent("Tango"),
+        RandomAgent("Uniform"),
+        RandomAgent("Victor"),
+        RandomAgent("Whiskey"),
+        RandomAgent("Xray"),
+        RandomAgent("Yankee"),
+        RandomAgent("Zulu"),
+    ]
+    
+    main(agents, save_txt=True, save_tsv=True)
