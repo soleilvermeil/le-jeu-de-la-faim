3b74420a9756944f7c322d04853877304274c614
JL <julien.leuenberger@outlook.com>
2025-01-16 15:46:49 +0100

    Added tooltip

--- game/core/cell.py
+++ game/core/cell.py
@@ -9,6 +9,29 @@ class Cell:
         weapon_proba_multiplier: int = 1,
         dangerous_weapon_proba: int = 0,
     ):
+        """
+        Create a new cell.
+
+        Parameters
+        ----------
+        name : str
+            The name of the cell.
+        icon : str
+            The icon of the cell, usually an emoji.
+        food_multiplier : int
+            If food is found in the cell, the amount of food found is
+            multiplied by this value.
+        water_multiplier : int
+            If water is found in the cell, the amount of water found is
+            multiplied by this value.
+        weapon_proba_multiplier : int
+            The probability of finding a weapon (instead of food or water) is
+            multiplied by this value.
+        dangerous_weapon_proba : int
+            If a weapon is found, the probability of that weapon being
+            dangerous.
+        
+        """
         self.name = name
         self.icon = icon
         self.food_multiplier = food_multiplier
